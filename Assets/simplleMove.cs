3d983d276c6e97a33ef6f6980c1a334c131ee5b5
Leal <18707681451@163.com>
2017-07-09 08:00:19 +0800

    RGP(unity3d)

---
+++ Assets/simplleMove.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simplleMove : MonoBehaviour {
+
+	void FixedUpdate () {
+	    transform.Translate(5*Time.fixedDeltaTime*transform.forward);
+	}
+}
